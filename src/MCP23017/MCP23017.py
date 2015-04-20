d11304be053c9e7751827e52f45d9c78dd8b1cf3
holz1 <tobias.ammann@tum.de>
2015-04-20 21:59:17 +0200

    First commit MCP23017 library
    first test pending

---
+++ src/MCP23017/MCP23017.py
@@ -0,0 +1,97 @@
+__author__ = 'holzi'
+
+#!/usr/bin/python
+import smbus
+
+# Defines
+IODIRA   = 0x00
+IODIRB   = 0x01
+IPOLA    = 0x02
+IPOLB    = 0x03
+GPINTENA = 0x04
+GPINTENB = 0x05
+DEFVALA  = 0x06
+DEFVALB  = 0x07
+INTCONA  = 0x08
+INTCONB  = 0x09
+IOCON    = 0x0A     # 0x0B points to the same register
+GPPUA    = 0x0C
+GPPUB    = 0x0D
+INTFA    = 0x0E
+INTFB    = 0x0F
+INTCAPA  = 0x10
+INTCAPB  = 0x11
+GPIOA    = 0x12
+GPIOB    = 0x13
+OLATA    = 0x14
+OLATB    = 0x15
+
+class MCP23017:
+    #constans
+    OUT = 0
+    IN = 1
+    LOW = 0
+    HIGH = 1
+
+    I2CPort = 0x00                  # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+    bus = 0x00                      #SMBUS-Object
+    devAddr = 0x00
+    devRegMode = 0x00
+
+
+def __init__(self, devaddr, i2cport = 1, devregmode = 0):
+
+    self.I2CPort = i2cport
+    self.bus = smbus.SMBus(self.I2CPort)
+    self.devAddr = devaddr
+    self.devRegMode = devregmode
+
+def initDevice(self):
+    pass
+    #TODO Reset ausfuehren
+
+
+def check_uint8(bit_pattern):
+    #check bit_pattern
+    if (bit_pattern >= 0) and (bit_pattern <= 0xFF):
+        return 0
+    else:
+        assert "Invalid Data, expected a one byte pattern"
+        return 1
+
+def set_io_direction(self, port, bit_pattern):
+    ''' Controls the direction of the data I/O. [0 - Output] [1 - Input] '''
+
+    if check_uint8(bit_pattern):
+        #check Port
+        if port == "PortA":
+            port_addr = IODIRA
+        elif port == "PortB":
+            port_addr = IODIRB
+        else:
+            assert "Invalid Port: Use \"A\" for PortA and \"B\" for PortB"
+
+        self.bus.write_byte_data(self.devAddr, port_addr, bit_pattern)
+
+
+def set_input_polarity(self, port, bit_pattern):
+    ''' If a bit is set, the corresponding GPIO register bit will
+    reflect the inverted value on the pin.'''
+
+    if check_uint8(bit_pattern):
+        #check Port
+        if port == "PortA":
+            port_addr = IPOLA
+        elif port == "PortB":
+            port_addr = IPOLB
+        else:
+            assert "Invalid Port: Use \"A\" for PortA and \"B\" for PortB"
+
+        self.bus.write_byte_data(self.devAddr, port_addr, bit_pattern)
+
+def read_byte_port_a(self):
+    return self.bus.read_byte_data(self.devAddr, GPIOA)
+
+
+def read_byte_port_b(self):
+    return self.bus.read_byte_data(self.devAddr, GPIOB)
---
+++ src/main.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+from MCP23017.MCP23017 import MCP23017
+
