1c94b808cac8ea17021c39be9fc4657693d524a0
Max Ammann <maximilian.ammann@googlemail.com>
2015-04-21 23:21:34 +0200

    Added web framework skeleton

---
+++ src/web/web.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template("lights.html")
+
+
+def start_web():
+    app.run()
